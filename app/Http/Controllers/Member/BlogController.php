5d8fc68427a13e3ec852233c74eb97a4551e9a7b
SitiDiyahWulandari <sitidiyahwulandari@gmail.com>
2025-01-10 11:31:29 +0700

    fungsi hapus

--- app/Http/Controllers/Member/BlogController.php
+++ app/Http/Controllers/Member/BlogController.php
@@ -128,7 +128,11 @@ class BlogController extends Controller
      */
     public function destroy(Post $post)
     {
-       //
+        if (isset($post->thumbnail) && file_exists(public_path(getenv('CUSTOM_TUMBNAIL_LOCATION')) . "/" . $post->thumbnail)) {
+            unlink(public_path(getenv('CUSTOM_TUMBNAIL_LOCATION')) . "/" . $post->thumbnail);
+        }
+        Post::where('id', $post->id)->delete();
+       return redirect()->route('member.blogs.index')->with('success', 'Data berhasil dihapus');
     }
 
     /**
